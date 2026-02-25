9f65b213735e48a00b2403d8055895a5ed525ab5
ymor-calayag <jeffreyymor.calayag@gmail.com>
2026-02-25 17:03:42 +0800

    Add: movement of turtle

---
+++ main.py
@@ -0,0 +1,26 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.move_up, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+
+
+
+screen.exitonclick()
---
+++ player.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+
+# STARTING_POSITION = (0, -280)
+# MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.starting_pos = (0, -280)
+        self.move_distance = 10
+        self.create_player()
+
+    def create_player(self):
+        self.shape("turtle")
+        self.up()
+        self.goto(self.starting_pos)
+        self.setheading(90)
+
+    def move_up(self):
+        self.fd(self.move_distance)
